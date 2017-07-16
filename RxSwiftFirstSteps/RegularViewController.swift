bf29c11e9c0cdf0b957c43c587d1c1fe6a3e77b0
Thiago Lioy <thiagolioy@gmail.com>
2017-07-16 13:08:01 -0300

    Create Infra for vcs using rxswift and the regular approach

---
+++ RxSwiftFirstSteps/RegularViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegularViewController.swift
+//  RxSwiftFirstSteps
+//
+//  Created by Thiago Lioy on 16/07/17.
+//  Copyright © 2017 Thiago Lioy. All rights reserved.
+//
+
+import UIKit
+
+class RegularViewController: UIViewController {
+
+}
